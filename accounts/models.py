40368b6bda96bdae242c0267f7a5db3c095a1184
Tomek Paczkowski <tomek@hauru.eu>
2011-12-05 22:51:17 +0100

    Clean up unused import

--- accounts/models.py
+++ accounts/models.py
@@ -2,15 +2,11 @@ import pytz
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
 TIME_ZONE_CHOICES = zip(pytz.common_timezones, pytz.common_timezones)
 
 class Profile(models.Model):
-    utc = pytz.utc
-    common_timezones = pytz.common_timezones
-
     user = models.OneToOneField(User)
     time_zone = models.CharField(max_length=200, choices=TIME_ZONE_CHOICES,
                                  blank=True)
